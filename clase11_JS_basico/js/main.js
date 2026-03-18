90f30a350ae049f48c23a48688ea9f7394732e90
Wilmer Arley Patiño Perdomo <wilmerarleypatinoperdomo@MacBook-Air-de-Wilmer.local>
2026-03-18 11:40:43 -0500

    JS basic class

---
+++ clase11_JS_basico/js/main.js
@@ -0,0 +1,18 @@
+let x=1, y="1", z="uno", m=[23, false, "texto", [2,true,4]], n=true, o=false;
+
+
+console.log(x + " es de tipo "+ typeof(x));
+console.log(y + " es de tipo "+ typeof(y));
+console.log(z + " es de tipo "+ typeof(z));
+console.log(m + " es de tipo "+ typeof(m));
+console.log(x === y);
+if(n && !o){
+    console.log("Iguales");    
+}else{
+    console.log("Diferentes");        
+}
+
+
+
+// let name= prompt("Ingrese su nombre")
+// alert("hi "+name);

a787404854c5741a102f88398fe129ccca2beee4
Wilmer Arley Patiño Perdomo <wilmerarleypatinoperdomo@MacBook-Air-de-Wilmer.local>
2026-04-06 11:39:46 -0500

    mui +  React

--- Clase12_React/src/App.jsx
+++ Clase12_React/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import './App.css';
 import Boton from './Components/Boton/Boton';
 import BotonMui from './Components/BotonMui/BotonMui';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>Hola Mundo <AccountBalanceIcon /> </h1>
       <Boton />
       <BotonMui />
     </>
--- Clase12_React/src/Components/BotonMui/BotonMui.jsx
+++ Clase12_React/src/Components/BotonMui/BotonMui.jsx
@@ -1,11 +1,30 @@
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
-const BotonMui = () => {
+export default function BotonMui() {
   return (
-    <Button variant="contained" disableElevation>
-      Disable elevation
-    </Button>
+    <Stack direction="row" spacing={2}>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Stack>
   );
 }
 
-export default BotonMui;
+
+// import Button from '@mui/material/Button';
+
+// const BotonMui = () => {
+//   return (
+//     <Button variant="contained" disableElevation>
+//       Disable elevation
+//     </Button>
+//   );
+// }
+
+// export default BotonMui;
